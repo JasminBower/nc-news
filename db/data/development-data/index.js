1715d7d399020ea8dcf905bc4f57186752735f98
JasminBower <jasmin.ibower@gmail.com>
2020-03-02 11:51:55 +0000

    index.js files set up

---
+++ db/data/development-data/index.js
@@ -0,0 +1,4 @@
+exports.articleData = require('./articles.js');
+exports.commentData = require('./comments.js');
+exports.userData = require('./users.js');
+exports.topicData = require('./topics.js')
---
+++ db/data/test-data/index.js
@@ -0,0 +1,4 @@
+exports.articleData = require('./articles.js');
+exports.topicData = require('./topics.js');
+exports.userData = require('./users.js');
+exports.commentData = require('./comments.js')
